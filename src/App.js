f84f240a14e538711e3f830ca3b9ba29a600d023
Subin <dmsbow_@daum.net>
2023-08-05 16:53:29 +0900

    Post 데이터 가져오기 예제

--- src/App.js
+++ src/App.js
@@ -1,41 +1,10 @@
-import React, {useState} from 'react';
-import axios from "axios";
+import React from 'react';
+import Test from "./pages/Test";
 
 const App = () => {
-    //1. 데이터 담을 그릇 선언.
-    const [newsList, setNewsList] = useState([]);
-
-    //2. 데이터를 가져오는 함수 선언.
-    const getNewsList = async () => {
-        //5.네트워크 로직.
-        try {
-
-            const address = "https://newsapi.org/v2/everything?q=tesla&from=2023-07-05&sortBy=publishedAt&apiKey=879f9f6b8c7e4c16939ccefca8aac63e"
-            const result = await axios.get(address)
-            console.log("++++++++++++++++++++++++++", result.data.articles)
-            setNewsList(result.data.articles);
-        } catch (err) {
-            console.log("======================", err.message);
-        }
-    }
-
-    //3. 데이터를 가져오는 버튼 선언.(버튼)
-    //4. 데이터의 갯수 display
-    //6. 데이터 매핑.
     return (
         <div>
-
-            <button onClick={getNewsList}>뉴스리스트 가져오기</button>
-            {newsList && newsList.map((news, index) => (
-                <div key={index}>
-                    <h1>
-                        {news.title}
-                    </h1>
-                    <h3>
-                        {news.description}
-                    </h3>
-                </div>
-            ))}
+            <Test/>
         </div>
     );
 };
--- src/pages/Test.js
+++ src/pages/Test.js
@@ -1,17 +1,42 @@
 import React, {useState} from 'react';
+import axios, {post} from "axios";
 
 const Test = () => {
-    // "", 0, {}, [], t/f
-    const [age, setAge] = useState(0)
 
-    const myage = () => {
-        setAge(23);
+    //1. 데이터를 담을 그릇 선언
+    const [posts, setPostsList] = useState([])
+
+    //2. 데이터를 가져오는 함수 선언.
+    const getPostsList = async () => {
+        //5.네트워크 로직.
+        try{
+            const address = "https://jsonplaceholder.typicode.com/posts";
+            const result = await axios.get(address);
+            console.log("%%%%%%%%%%%%%%%%", result.data);
+            setPostsList(result.data);
+        } catch (err) {
+            console.log("################", err.message);
+        }
     }
 
+    //3. 데이터를 가져오는 버튼 선언 (버튼)
+    //4. 데이터의 갯수 display.
     return (
         <div>
-            <h1>{age}</h1>
-            <button onClick={myage}>나이바꾸기</button>
+            <button onClick={getPostsList}>포스트리스트 가져오기.</button>
+            <h1>
+                {posts.length}개의 데이터가 있습니다.
+            </h1>
+            {posts && posts.map((post, index) => (
+                <div key={index}>
+                    <h1>
+                        {post.title}
+                    </h1>
+                    <h3>
+                        {post.body}
+                    </h3>
+                </div>
+            ))}
         </div>
     );
 };
