672cb774fc2082f996d56274f5321cc5d76cf3d9
Subin <dmsbow_@daum.net>
2023-08-05 16:01:26 +0900

    Object를 가져오는 예제

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
 import React, {useState} from 'react';
 
 const App = () => {
-    const [movies, setMovies] = useState([])
+    const [movies, setMovies] = useState({})
 
     const inputMoviedata = () => {
-        const data = [
-            {
+        const data = {
                 title: "Movie1",
-                desc: "너무재밌어",
+                desc: [0, 3, 4, 10],
                 rate: 4.3,
-            },
-            {
-                title: "Movie2",
-                desc: "너무재미없음",
-                rate: 2.1,
-            },
-            {
-                title: "Movie3",
-                desc: "그냥그럼",
-                rate: 3.0,
             }
-        ]
+        // const data = [
+        //     {
+        //         title: "Movie1",
+        //         desc: "너무재밌어",
+        //         rate: 4.3,
+        //     },
+        //     {
+        //         title: "Movie2",
+        //         desc: "너무재미없음",
+        //         rate: 2.1,
+        //     },
+        //     {
+        //         title: "Movie3",
+        //         desc: "그냥그럼",
+        //         rate: 3.0,
+        //     }
+        // ]
 
         setMovies(data);
     }
@@ -28,14 +33,30 @@ const App = () => {
     return (
         <div>
             <button onClick={inputMoviedata}>데이터 불러오기!</button>
-            {movies && movies.map(movie => (
-                <div>
-                    <h1>{movie.title}</h1>
-                    <h3>{movie.desc}</h3>
-                </div>
-            ))}
+            <h1>
+                {movies.title}
+            </h1>
+            <h1>
+                {/*{movies.desc && movies.desc.map(i => (*/}
+                {/*    <h3>{i},</h3>*/}
+                {/*))}*/}
+                {movies.desc[1]}
+            </h1>
+            {/*{movies && movies.map(movie => (*/}
+            {/*    <div>*/}
+            {/*        <h1>{movie.title}</h1>*/}
+            {/*        <h3>{movie.desc}</h3>*/}
+            {/*    </div>*/}
+            {/*))}*/}
         </div>
     );
 };
 
 export default App;
+
+//코드 빈도 순서.
+//1. 위에서 아래 순서로 빌드한다.
+//2. =를 기준으로, 우측에서 좌측으로 치환.
+//3. .은 하위 메서드를 호출하는 명령어.
+//4. ,는 '그리고'로 해석한다.
+//5. ()는 함수들의 모음.
