a4d7992df9d0a53645b68723316eceda9defc1b4
Subin <dmsbow_@daum.net>
2023-08-12 11:02:00 +0900

    [Add] Detail 분기처리 (Movie, Tv)

--- src/pages/Detail.js
+++ src/pages/Detail.js
@@ -1,21 +1,59 @@
-import React from 'react';
-import {useParams} from "react-router-dom";
+import React, {useEffect, useState} from 'react';
+import {useLocation, useNavigate, useParams} from "react-router-dom";
+import axios from "axios";
+import {Button, Container} from "react-bootstrap";
 
 const Detail = () => {
+
+    const location = useLocation();
+    const navigate = useNavigate();
+    console.log("+++++++++++++++++++++++",location.pathname.includes("movie"));
+
+    const provider = location.pathname.includes("movie") ? "movie" : "tv";
 
     const {movieId, tvId} = useParams()
 
     const id = movieId ? movieId : tvId;
 
-    // url: 'https://api.themoviedb.org/3/tv/213713?language=en-US',
-    //     headers: {
-    //     accept: 'application/json',
-    //         Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4MGRkZDE3ODg2YzM5YWQwNGYyMTA4YzU4YzdhYjMyNSIsInN1YiI6IjY0Y2UwMDEwZDlmNGE2MDNiNDlmZGQyMiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.OAkt0tW_dGyVk-7QLJj8u_vZbaRTduN23FMmUp7o98Q'
-    // }
+    const [item, setItem] = useState({});
+
+    const getOTTdata = async () => {
+        try{
+            const address = `https://api.themoviedb.org/3/${provider}/${id}?language=en-US`
+            const options = {
+                headers: {
+                    accept: 'application/json',
+                    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4MGRkZDE3ODg2YzM5YWQwNGYyMTA4YzU4YzdhYjMyNSIsInN1YiI6IjY0Y2UwMDEwZDlmNGE2MDNiNDlmZGQyMiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.OAkt0tW_dGyVk-7QLJj8u_vZbaRTduN23FMmUp7o98Q'
+                }
+            }
+
+            const result = await axios.get(address, options);
+
+            console.log("##################", result.data);
+
+            setItem(result.data);
+        }
+        catch (err){
+            console.log(err.message);
+        }
+    }
+
+    useEffect(() => {
+        getOTTdata();
+    }, []);
+
     return (
-        <h1>
-            {id}
-        </h1>
+        <Container>
+            <Button onClick={() => navigate(-1)}>
+                뒤로가기
+            </Button>
+            <h1>
+                {item.title ? item.title : item.name};
+            </h1>
+            <h3>
+                {item.overview}
+            </h3>
+        </Container>
     );
 };
 
